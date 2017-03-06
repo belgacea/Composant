9c0680d3029141d380fd04472364a182a1c1d427
belgacea <belgacea@users.noreply.github.com>
2017-03-06 11:41:40 +0100

    Update Hacheur.h

--- Hacheur.h
+++ Hacheur.h
@@ -2,9 +2,15 @@
 #define Hacheur_h
 
 /**
-*Prend en entrée une chaine de caractères
-* La fonction doit retourner le hashage de la chaine de caractères en entrè en utilisant l'algorithme SHA-256 (64 bits en sortie) 
-**/
-std::string hash(std::string block);
+* Prend en entrée une chaine de caractères
+* Retourne le hash de la chaine de caractères en entrée en utilisant l'algorithme SHA-256 (64 bits en sortie) 
+*/
+std::string hash(std::string chaine);
+
+/**
+* Prend en entrée une chaine de caractères et un hash (chaine de caractères)
+* Vérifie que le hash de la chaine correspond bien au hash en entrée
+*/
+std::bool validateHash(std::string chaine, std::string hash);
 
 #endif

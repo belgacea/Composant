1baab2109eac71f4a77358b50f4133b8339161e8
belgacea <belgacea@users.noreply.github.com>
2017-03-06 11:09:14 +0100

    Update Hacheur.h

--- Hacheur.h
+++ Hacheur.h
@@ -2,12 +2,9 @@
 #define Hacheur_h
 
 /**
-*Prend en entrée:
-	- Le nouveau bloc d'opérations
-	- L'identifiant du bloc pérécédent
-	- Le nonce du nouveau bloc
-* La fonction doit retourner le hashage des paramétres en entrèes en utilisant le hashage SHA-256 (64 bits en sortie) 
+*Prend en entrée une chaine de caractères
+* La fonction doit retourner le hashage de la chaine de caractères en entrè en utilisant l'algorithme SHA-256 (64 bits en sortie) 
 **/
-std::string hach(std::string newBlock, std::string previousBlockId, std::string nonce);
+std::string hach(std::string block);
 
 #endif

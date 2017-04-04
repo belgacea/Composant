1fae9ea5b7718064ca62f634d35ea1a7fb732e42
belgacea <belgacea@users.noreply.github.com>
2017-04-04 14:12:57 +0200

    Update Hacheur.h

--- Hacheur.h
+++ Hacheur.h
@@ -2,6 +2,7 @@
 #ifndef Hacheur_h
 #define Hacheur_h
 
+using namespace std;
 #include <string>
 #include "Bloc.h"
 
@@ -9,18 +10,18 @@
 * Prend en entrée une chaine de caractères
 * Retourne le hash de la chaine de caractères en entrée en utilisant l'algorithme SHA-256 (256 bits en sortie) 
 */
-std::string hash(std::string chaine);
+string hash(string chaine);
 
 /**
 * Prend en entrée une chaine de caractères et un hash (chaine de caractères)
 * Vérifie que le hash de la chaine correspond bien au hash en entrée
 */
-std::bool checkHash(std::string chaine, std::string hash);
+bool checkHash(string chaine, string hash);
 
 /**
 * Prend en entrée un bloc
 * Retourne une chaine de caractères contenant les données du bloc dans un format quelconque
 */
-std::string blocToString(Bloc bloc);
+string blocToString(Bloc bloc);
 
 #endif
